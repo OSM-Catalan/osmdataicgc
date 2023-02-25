d15b4a19fb82ce6b9bbbb68dd6b16d6338284221
yopaseopor <yopaseopor@gmail.com>
2023-02-25 12:31:56 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -176,6 +176,43 @@ var config = {
 				
 		
 		{	
+			group: 'OSM',
+			title: 'Límits municipals',
+			query: '(nwr["admin_level"="8"]({{bbox}});node(w););out meta;',
+			iconSrc: 'icones/maxspeed_empty.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+			
+		{
 			group: 'Marcas & Restauración',
 			title: 'Burger King',
 			query: '(nwr["brand:wikidata"="Q177054"]({{bbox}});node(w););out meta;',
