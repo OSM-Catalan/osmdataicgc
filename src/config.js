3ad7c17393164e1c48e5ba816cc969c12c56b69c
yopaseopor <yopaseopor@gmail.com>
2023-02-27 20:53:59 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -223,44 +223,7 @@ style: function (feature) {
 				return style;
 			}
 				},
-				
-		{	
-			group: 'Test',
-			title: 'Amb nom (node)',
-			query: '(nwr["name"]({{bbox}});way(r)({{bbox}});node(w););out meta;',
-			iconSrc:  imgSrc + 'base/circle.svg',
-			iconStyle: 'background-color:rgba(255,255,255,0.4)',
-style: function (feature) {
-				var key_regex = /^name$/
-				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
-				var name = feature.get(name_key) || '';
-				var fill = new ol.style.Fill({
-					color: 'rgba(255,0,0,0.4)'
-				});
-				var stroke = new ol.style.Stroke({
-					color: 'rgba(255,0,0,1)',
-					width: 1
-				});
-				var style = new ol.style.Style({
-					image: new ol.style.Icon({
-							src: imgSrc + 'base/circle.svg',
-							scale:0.03
-						}),
-							text: new ol.style.Text({
-								text: name,
-								offsetX : 7,
-								offsetY : -12,
-								fill: new ol.style.Fill({
-                            color: 'rgba(255,0,0,1)'
-                        }),
-						}),
-					fill: fill,
-					stroke: stroke
-				});
-				return style;
-			}
-				},
-				
+						
 		{	
 			group: 'OSM',
 			title: 'Amb nom (node)',
@@ -738,6 +701,43 @@ style: function (feature) {
 								offsetY : -12,
 								fill: new ol.style.Fill({
                             color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+				
+		{	
+			group: 'Test',
+			title: 'Amb nom (node)',
+			query: '(nwr["name"]({{bbox}});way(r)({{bbox}});node(w););out meta;',
+			iconSrc:  imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'base/circle.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(255,0,0,1)'
                         }),
 						}),
 					fill: fill,
